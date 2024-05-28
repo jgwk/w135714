d3e983fecd09ecbd63b78bb8e661398bca540677
jgwk <junglewalker99@gmail.com>
2024-05-28 19:36:31 +0900

    update verticle to pub/sub

--- vertx-spring/src/main/java/jgwk/verticle/BlogVerticle.java
+++ vertx-spring/src/main/java/jgwk/verticle/BlogVerticle.java
@@ -1,6 +1,7 @@
 package jgwk.verticle;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -18,34 +19,58 @@ import java.util.List;
 public class BlogVerticle extends AbstractVerticle {
     private final BlogService blogService;
 
-//    private static final String POST_GET_ALL = "post.get.all";
-//    private static final String POST_ADD = "post.add";
-//    private static final String POST_UPDATE = "post.update";
-//    private static final String POST_DELETE = "post.delete";
+    private static final String POST_GET_ALL = "post.get.all";
+    private static final String POST_ADD = "post.add";
 
+    // TODO 다큐먼트가 직접 노출되지 않도록, 매퍼 추가
     @Override
     public void start() {
         Router router = Router.router(vertx);
-
         router.route().handler(BodyHandler.create());
 
         // 글 전체 조회
         router
             .get("/api/post")
-            .handler(context -> {
-                List<Post> posts = blogService.getAllPosts();
+            .handler(context -> vertx.eventBus()
+                .<String>request(POST_GET_ALL, null, message -> {
+                    if (message.succeeded()) {
+                        context
+                            .response()
+                            .end(message.result().body());
+                    } else {
+                        context
+                            .response()
+                            .setStatusCode(500)
+                            .end();
+                    }
+            }));
 
-                context.json(posts);
+        vertx.eventBus()
+            .consumer(POST_GET_ALL, message -> {
+                List<Post> postList = blogService.getAllPosts();
+                message.reply(Json.encode(postList));
             });
-
-        // TODO 이벤트 모델로 변경
-        // TODO 다큐먼트가 직접 노출되지 않도록, 매퍼 추가
 
         // 글 등록
         router
             .post("/api/post")
-            .handler(context -> {
-                JsonObject postJson = context.getBodyAsJson();
+            .handler(context -> vertx.eventBus()
+                .<String>request(POST_ADD, context.getBodyAsJson(), message -> {
+                    if (message.succeeded()) {
+                        context
+                            .response()
+                            .end(message.result().body());
+                    } else {
+                        context
+                            .response()
+                            .setStatusCode(500)
+                            .end();
+                    }
+                }));
+
+        vertx.eventBus()
+            .consumer(POST_ADD, message -> {
+                JsonObject postJson = (JsonObject) message.body();
 
                 Post post = new Post();
                 post.setPostId(System.currentTimeMillis());
@@ -53,8 +78,7 @@ public class BlogVerticle extends AbstractVerticle {
                 post.setContent(postJson.getString("content"));
 
                 blogService.addPost(post);
-
-                context.json(post);
+                message.reply(Json.encode(post));
             });
 
         // 글 수정
